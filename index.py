074aa6b35bfb4db263d1b1d18bd640923088293d
Naoky-X <sergioanruizh1228@gmail.com>
2025-04-30 12:35:23 -0500

    Talleres python

---
+++ index.py
@@ -0,0 +1,172 @@
+#Taller 1################################################################################3
+
+# edad = input("Introduce tu edad: ")
+# edad = int(edad)
+# print("Tu edad es: " , edad)
+
+# peso = input("Introduce tu Peso: ")
+# peso = int(peso)
+# print("Tu peso es: " , peso)
+
+
+###################################
+
+# base = input("digite la base: ")
+# base = int(base)
+
+# altura = input("digite la altura: ")
+# altura = int(altura)
+
+# area =(base*altura)/2
+
+# print("El area de su triangulo es: ", area)
+
+
+###########################################
+
+# radio = input("digite el radio: ")
+# radio = int(radio)
+
+
+
+# area =(radio*radio)*3.1416
+
+# print("El area de su Circulo es: ", area) 
+
+#############################################
+
+# peso = input("Introduce tu peso: ")
+# peso = int(peso)
+
+# pesoL= (peso* 2.2046)
+# print("Tu peso en libras es: " , pesoL)
+
+####################################################
+
+# TotalC = int (input("Introduce el valor total de tu compra: "))
+
+# Descuento =(TotalC*0.1)
+# Total = (TotalC-Descuento)
+
+# print("Su descuento en total es: ", Total)
+
+#####################################################
+
+# Nota1 = int(input("introduce tu 1 nota: ")) 
+# Nota2 = int(input("introduce tu 2 nota: ")) 
+# Nota3 = int(input("introduce tu 3 nota: ")) 
+# Nota4 = int(input("introduce tu 4 nota: ")) 
+
+# Porcentaje1 = 0.3
+# Porcentaje2 = 0.25
+# Porcentaje3 = 0.15
+
+
+# TotalNotas = ((Nota1*Porcentaje1)+(Nota2*Porcentaje1)+(Nota3*Porcentaje2)+(Nota4*Porcentaje3))
+# print("su nota final es: " , TotalNotas)
+
+##################################################################################################
+
+# A = int(input("introduce el primer valor: "))
+# B = int(input("introduce el segundo valor: "))
+
+# Resultado = ((A+B)**2)/3
+
+# print("El resultado de la operacion dada es: ",Resultado)
+
+################################################################################
+
+# NumDias = int(input("Intruduce el numero de dias: "))
+
+# Resultados = (NumDias*86400)
+
+# print ("el numero de segundos que hay en esos dias es: ", Resultados)
+
+################################################################################
+
+# Valor1 = int(input("introduce ti valor 1: "))
+# Valor2 = int(input("introduce ti valor 2: "))
+# Valor3 = int(input("introduce ti valor 3: "))
+# Valor4 = int(input("introduce ti valor 4: "))
+
+# Multi= (Valor1*Valor2*Valor3*Valor4)
+# print ("el resultado de la multiplicacion de todos los valores es: ", Multi)
+# Suma=(Valor1+Valor2+Valor3+Valor4)
+# print ("el resultado de la Suma de todos los valores es: ", Suma)
+# Prome=(Suma/4)
+# print ("el resultado del promedio de todos los valores es: ", Prome)
+
+#######################################################################################
+
+# SalHora = int(input("Dame tu precio de hora trabajada: "))
+# HoraT= int(input("Dame las horas trabajadas: "))
+
+# TotalS = (SalHora*HoraT)
+# RoboBoss = (TotalS*0.08)
+# SalarioN=(TotalS-RoboBoss)
+
+# print("tu salario en total es: ",SalarioN)
+
+###############################################################################################
+
+
+
+
+# Taller 2 ###############################################################################
+
+# Numero =int(input("Dame un numero: "))
+
+# if(Numero%2==0):
+#     print("El Numero es Par")
+# else:
+#     print("El numero es Impar")
+
+###########################################################################################
+
+# numero = float(input("Ingresa un número: "))
+
+# if numero > 0:
+#   print("El número es positivo.")
+# else:
+#     print("El número es negativo.")
+
+#############################################################################################
+
+# nivel = int(input("ingresa la temperatura del agua"))
+# if nivel < 0 :
+#     print("agua en estado solido")
+# elif  nivel < 100:
+#     print("agua en estado liquido")
+# else :
+#     print("agua en estado gaseoso")
+
+################################################################################################
+
+# año = int (input("ingresa un año"))
+# if  año % 4 == 0 and año % 100 != 0:
+#     print("este numero es bisiesto")
+# else :
+#     print("este año no es bisiesto")
+
+###############################################################################################
+
+# horas = float(input("Introduce el número de horas trabajadas en la semana: "))
+# precio_hora = float(input("Introduce el precio por hora (€): "))
+# if horas <= 35:
+#     salario_bruto = horas * precio_hora
+# else:
+#     horas_extras = horas - 35
+#     salario_bruto = (35 * precio_hora) + (horas_extras * precio_hora * 1.25)
+# salario_mensual = salario_bruto * 4
+# if salario_mensual < 1000:
+#     impuestos = 0
+# elif salario_mensual <= 2000:
+#     impuestos = 0.20 * salario_bruto
+# else:
+#     impuestos = 0.30 * salario_bruto
+# salario_neto = salario_bruto - impuestos
+# print("\nSalario bruto semanal:", salario_bruto, "€")
+# print("Impuestos semanales:", impuestos, "€")
+# print("Salario neto semanal:", salario_neto, "€")
+
+########################################################################################################
