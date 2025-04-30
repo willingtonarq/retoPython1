0da9dd2b31bb59b9cf0d159cd0d8c223a3ab94e3
Windfull13 <angelo.franco36939@ucaldas.edu.co>
2025-04-30 12:42:49 -0500

    Taller Python

---
+++ tallercondicionales.py
@@ -0,0 +1,67 @@
+# "ej 1"
+
+# numero=input("Introduce un número: ")
+# numero=int(numero)
+
+# if numero%2==0:
+#     print("El número es par")
+# else:
+#     print("El número es impar")
+
+# "ej 2"
+
+# numero=input("Introduce un número: ")
+# numero=int(numero)
+
+# if numero>0:
+#     print("El número es positivo")
+# elif numero<0:
+#     print("El número es negativo")
+# else:
+#     print("El número es cero")/*+9-
+
+# "ej 3"
+# temperatura=input("Introduce la temperatura en grados Celsius: ")
+# temperatura=float(temperatura)
+
+# if temperatura<0:
+#     print("El agua está en estado sólido")
+# elif temperatura>=0 and temperatura<100:
+#     print("El agua está en estado líquido")
+# else:}
+#     print("El agua está en estado gaseoso")
+
+# "ej 4"
+
+# año=input("Introduce un año: ")
+# año=int(año)
+
+# if año%4==0 and año%100!=0:
+#     print("El año es bisiesto")
+# else:
+#     print("El año no es bisiesto")
+
+
+# "ej 5"
+
+# horastrabajadas=input("Introduce las horas trabajadas: ")
+# preciohora=input("Introduce el precio por hora: ")
+
+# if horastrabajadas <=35:
+#     salariobruto=horastrabajadas*preciohora
+# else:
+#     horasextras=horastrabajadas-35
+#     salariobruto=35*preciohora+(horasextras*preciohora*1.25)
+
+# salariomensual=salariobruto*4
+
+# if salariomensual<1000:
+#     impuestos=0
+# elif salariomensual>=1000 and salariomensual<=2000:    
+#     impuestos=salariobruto*0.2
+# else:
+#     impuestos=salariobruto*0.3
+
+# salarioneto=salariobruto-impuestos     
+
+# print("El salario neto del trabajador es: ", salarioneto) 
---
+++ talleroperadores.py
@@ -0,0 +1,116 @@
+# "ej #1"
+
+# edad=input("Introduce tu edad: ")
+# edad=int(edad)
+# peso=input("Introduce tu peso: ")
+# peso=int(peso)
+
+# print("Tu edad es: ", edad)
+# print("Tu peso es: ", peso)
+
+# "ej #2"
+
+# base=input("Introduce la base del triángulo: ")
+# base=int(base)
+# altura=input("Introduce la altura del triángulo: ")
+# altura=int(altura)
+
+# area=base*altura/2
+
+# print("El área del triángulo es: ", area)
+
+# "ej #3"
+
+# radio=input("Introduce el radio del círculo: ")
+# radio=int(radio)
+
+# area=3.14*(radio**2)
+
+# print("El área del círculo es: ", area "cm")
+
+# "ej #4"
+
+# kilogramos=input("Introduzca su peso en kilogramos para convertir a libras: ")
+# kilogramos=int(kilogramos)
+
+# libras=kilogramos*2.205
+
+# print("Su peso en libras es: ", libras, "lb")
+
+# "ej #5"
+
+# compra=input("Introduce el precio de la compra: ")
+# compra=float(compra)
+
+# descuento=compra*0.10
+# total=compra-descuento
+
+# print("El descuento aplicado es: ", descuento, "€")
+# print("El total a pagar es: ", total, "€")  """
+
+# "ej #6"
+
+# nota1=input("Introduce la nota 1: ")
+# nota1=float(nota1)
+# nota2=input("Introduce la nota 2: ")
+# nota2=float(nota2)
+# nota3=input("Introduce la nota 3: ")
+# nota3=float(nota3)
+# nota4=input("Introduce la nota 4: ")
+# nota4=float(nota4)
+
+# notafinal=((nota1*0.3)+(nota2*0.3)+(nota3*0.25)+(nota4*0.15))
+
+# print("La nota final es: ", notafinal)
+
+# "ej #7"
+
+# numeroA=input("Introduce el primer número: ")
+# numeroA=int(numeroA)
+# numeroB=input("Introduce el segundo número: ")
+# numeroB=int(numeroB)
+
+# resultadoexpresion=((numeroA+numeroB)**2)/3
+
+# print("El resultado de la expresión ((A+B)^2)/3  con los números dados es: ", resultadoexpresion)
+
+
+# "ej #8"
+
+# dias=input("Introduce el número de días: ")
+# dias=int(dias)
+
+# segundos=dias*24*60*60
+
+# print("El número de segundos en", dias , "días es:", segundos, "s")
+
+# "ej #9"
+
+# numero1=input("Introduce el primer número: ")
+# numero1=int(numero1)
+# numero2=input("Introduce el segundo número: ")
+# numero2=int(numero2)
+# numero3=input("Introduce el tercer número: ")
+# numero3=int(numero3)
+# numero4=input("Introduce el cuarto número: ")
+# numero4=int(numero4)
+
+# suma=numero1+numero2+numero3+numero4
+# producto=numero1*numero2*numero3*numero4
+# promedio=suma/4
+
+# print("La suma de los números dados es: ", suma)
+# print("El producto de los números dados es: ", producto)
+# print("El promedio de los números dados es: ", promedio)
+
+# "ej #10"
+
+# numerohoras=input("Introduce el número de horas trabajadas: ")
+# numerohoras=int(numerohoras)
+# preciohora=input("Introduce el precio por hora: ")
+# preciohora=int(preciohora)
+
+# salarioneto=numerohoras*preciohora
+# descuento=salarioneto*0.8
+
+# print("El salario neto del trabajador es: ", salarioneto, "€")
