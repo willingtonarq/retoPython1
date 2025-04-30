171ec409424b364c756db102a13c27c276cbbe98
andresmchc456 <andresmchc456@gmail.com>
2025-04-30 12:43:20 -0500

    ejercicios

--- condicionales.py
+++ condicionales.py
@@ -1,26 +1,21 @@
-# num=int(input("Ingrese un dijito: "))
-# if num%2 ==0:
-#     print(f"el numero es par {num}")
-# else:
-#     print(f"el numero es impar {num}")
+num=int(input("Ingrese un dijito: "))
+if num%2 ==0:
+    print(f"el numero es par {num}")
+else:
+    print(f"el numero es impar {num}")
  
 
-# num=int(input("Ingrese un dijito: "))
-# if num >0 :
-#     print(f"el numero es positivo {num}")
-# else:
-#     print(f"el numero es negativo {num}")    
+num=int(input("Ingrese un dijito: "))
+if num >0 :
+    print(f"el numero es positivo {num}")
+else:
+    print(f"el numero es negativo {num}")    
 
 
-# temperatura=int(input("ingresa la temperatura: "))
-# if temperatura < 0:
-#     print(f"SOLIDO {temperatura}")
-# elif temperatura < 100:
-#     print(f"liquido {temperatura}")
-# elif temperatura>100:
-#     print(f"gaseoso {temperatura}")        
-
-
-
-# año=int(input("ingrese el año"))
-# if (año % 4 == 0 and año % 100 != 0) or (año % 400 == 0):
+temperatura=int(input("ingresa la temperatura: "))
+if temperatura < 0:
+    print(f"SOLIDO {temperatura}")
+elif temperatura < 100:
+    print(f"liquido {temperatura}")
+elif temperatura>100:
+    print(f"gaseoso {temperatura}")        
